ac8019a10c02e9c33eb0c315f4dd6843df3cd014
Veervikram singh <veervikramsingh132@gmail.com>
2022-11-19 13:26:20 +0530

    Smart contract

---
+++ hardhat/contracts/CryptoDevToken.sol
@@ -0,0 +1,60 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./ICryptoDevs.sol";
+
+contract CryptoDevToken is ERC20, Ownable {
+    uint public constant tokenPrice = 0.001 ether;
+    uint256 public constant tokensPerNFT = 10 * 10**18;
+    uint256 public constant maxTokenSupply = 10000 * 10**18;
+    ICryptoDevs CryptoDevsNFT;
+    mapping(uint256 => bool) public tokenIdsClaimed;
+
+    constructor(address _cryptoDevContract) ERC20("Crypto Dev Token", "CD") {
+        CryptoDevsNFT = ICryptoDevs(_cryptoDevContract);
+    }
+
+    function mint(uint256 amount) public payable {
+        uint256 _requiredAmount = tokenPrice * amount;
+        require(msg.value >= _requiredAmount, "Ether sent is incorrect");
+        uint256 amountWithDecimals = amount * 10**18;
+        require(
+            totalSupply() + amountWithDecimals <= maxTokenSupply,
+            "Exceeds the max total supply available"
+        );
+        _mint(msg.sender, amountWithDecimals);
+    }
+
+    function claim() public {
+        address sender = msg.sender;
+        uint256 balance = CryptoDevsNFT.balanceOf(sender);
+        require(balance > 0, "you don't own any Crypto Dev NFT");
+
+        // the below code is for the case where a sender can't mint multiple
+        // token for a single NFT
+        uint256 amount = 0;
+        for (uint256 i = 0; i < balance; i++) {
+            uint256 tokenId = CryptoDevsNFT.tokenOfOwnerByIndex(sender, i);
+            if (!tokenIdsClaimed[tokenId]) {
+                amount += 1;
+                tokenIdsClaimed[tokenId] = true;
+            }
+        }
+        require(amount > 0, "youy have already claimed Crypto Dev Tokens");
+
+        _mint(msg.sender, amount * tokensPerNFT);
+    }
+
+    function withdraw() public onlyOwner {
+        address _owner = owner();
+        uint256 amount = address(this).balance;
+        (bool sent, ) = _owner.call{value: amount}("");
+        require(sent, "Failed to send ether");
+    }
+
+    receive() external payable {}
+
+    fallback() external payable {}
+}
