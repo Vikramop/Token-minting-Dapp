28078ab7939d76af4e0a6cb6379c2308c1c8cd5a
Veervikram singh <veervikramsingh132@gmail.com>
2022-11-21 12:43:59 +0530

    Done

--- front-end/pages/index.js
+++ front-end/pages/index.js
@@ -1,8 +1,9 @@
 import Head from 'next/head';
 import { useEffect, useRef, useState } from 'react';
 import styles from '../styles/Home.module.css';
-import Web3Modal, { providers } from 'web3modal';
-import { BigNumber, utils } from 'ethers';
+import Web3Modal from 'web3modal';
+import { BigNumber, Contract, utils, providers } from 'ethers';
+import { TOKEN_CONTRACT_ADDRESS, TOKEN_CONTRACT_ABI } from '../constants';
 
 export default function Home() {
   const zero = BigNumber.from(0);
@@ -12,12 +13,14 @@ export default function Home() {
   const [balanceOfCryptoDevTokens, setBalanceOfCryptoDevTokens] =
     useState(zero);
   const [tokenAmount, setTokenAmount] = useState(zero);
+  const [loading, setLoading] = useState(false);
 
   const getProviderOrSigner = async (needSigner = false) => {
     const provider = await web3ModalRef.current.connect();
-    const web3Provider = new providers.web3Provider(provider);
 
-    const { chainId } = web3Provider.getNetwork();
+    const web3Provider = new providers.Web3Provider(provider);
+
+    const { chainId } = await web3Provider.getNetwork();
 
     if (chainId !== 80001) {
       window.alert('Change the network to polygon Testnet');
@@ -40,7 +43,67 @@ export default function Home() {
     }
   };
 
+  const getBalanceOfCryptoDevTokens = async () => {
+    try {
+      const provider = await getProviderOrSigner();
+      const tokenContract = new Contract(
+        TOKEN_CONTRACT_ABI,
+        TOKEN_CONTRACT_ADDRESS,
+        provider
+      );
+      const signer = getProviderOrSigner(true);
+      const address = signer.getAddress();
+      const balance = await tokenContract.balanceOf(address);
+      setBalanceOfCryptoDevTokens(balance);
+    } catch (err) {
+      console.error(err);
+      setBalanceOfCryptoDevTokens(zero);
+    }
+  };
+  const getTotalTokenMinted = async () => {
+    try {
+      const provider = await getProviderOrSigner();
+      const tokenContract = new Contract(
+        TOKEN_CONTRACT_ABI,
+        TOKEN_CONTRACT_ADDRESS,
+        provider
+      );
+      const _tokenMinted = await tokenContract.totalSupply();
+      setTokensMinted(_tokenMinted);
+    } catch (err) {
+      console.error(err, 'he');
+    }
+  };
+
+  const mintCryptoDevToken = async (amount) => {
+    try {
+      const signer = await getProviderOrSigner(true);
+      const tokenContract = new Contract(
+        TOKEN_CONTRACT_ABI,
+        TOKEN_CONTRACT_ADDRESS,
+        signer
+      );
+
+      const value = 0.001 * amount;
+
+      const tx = await tokenContract.mint(amount, {
+        value: utils.parseEther(value.toString()),
+      });
+
+      setLoading(true);
+      await tx.wait();
+      setLoading(false);
+      window.alert('Successfully minted Crypto Dev Token');
+      await getBalanceOfCryptoDevTokens();
+      await getTotalTokenMinted();
+    } catch (err) {
+      console.error(err);
+    }
+  };
   const renderButton = () => {
+    if (loading) {
+      return <div className={styles.button}>Loading...</div>;
+    }
     return (
       <div style={{ display: 'flex-col' }}>
         <div>
@@ -48,11 +111,16 @@ export default function Home() {
             type="number"
             placeholder="Amount of Tokens"
             onChange={(e) => setTokenAmount(BigNumber.from(e.target.value))}
+            className={styles.input}
           />
-          <button className={styles.button} disabled={!tokenAmount}>
-            Mint Tokens
-          </button>
         </div>
+        <button
+          className={styles.button}
+          disabled={!tokenAmount}
+          onClick={() => mintCryptoDevToken(tokenAmount)}
+        >
+          Mint Tokens
+        </button>
       </div>
     );
   };
@@ -66,8 +134,10 @@ export default function Home() {
       });
 
       connectWallet();
+      getBalanceOfCryptoDevTokens();
+      getTotalTokenMinted();
     }
-  }, []);
+  }, [walletConnected]);
 
   return (
     <div>
@@ -90,7 +160,7 @@ export default function Home() {
               <div className={styles.description}>
                 Overall {utils.formatEther(tokensMinted)}/10000 have been minted
               </div>
-              {renderButton}
+              {renderButton()}
             </div>
           ) : (
             <button onClick={connectWallet} className={styles.button}>
